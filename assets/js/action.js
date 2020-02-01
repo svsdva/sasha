22786649ff3c233aab07ae56a1ba84db8e442466
svsdva <53815820+svsdva@users.noreply.github.com>
2020-02-01 13:45:28 +0300

    Add files via upload

--- assets/js/action.js
+++ assets/js/action.js
@@ -9,3 +9,25 @@ row1.addEventListener("click", function () {
         row1_t.style.display = "none";
     }
 });
+
+var row2 = document.getElementById('row2');
+var row2_t = document.getElementById('row2_text');
+
+row2.addEventListener("click", function () {
+    if (getComputedStyle(row2_t).display == "none") {
+        row2_t.style.display = "inherit";
+    } else {
+        row2_t.style.display = "none";
+    }
+});
+
+var row3 = document.getElementById('row3');
+var row3_t = document.getElementById('row3_text');
+
+row3.addEventListener("click", function () {
+    if (getComputedStyle(row3_t).display == "none") {
+        row3_t.style.display = "inherit";
+    } else {
+        row3_t.style.display = "none";
+    }
+});
